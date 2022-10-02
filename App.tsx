3d2c4cc1ad3d021623f47641e390ec3c976f1405
Bogdan Ioanas <ioanasbogdan@gmail.com>
2022-10-02 23:16:31 +0300

    custom button

--- App.tsx
+++ App.tsx
@@ -1,20 +1,59 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	type Props = {
+		title: string;
+		onPress: () => void;
+		bgColor?: string;
+		textColor?: string;
+	};
+
+	const CustomButton = (props: Props) => {
+		const { bgColor = '#000', onPress, textColor = '#fff', title } = props;
+
+		return (
+			<TouchableOpacity
+				onPress={onPress}
+				style={[
+					styles.btn,
+					{
+						backgroundColor: bgColor,
+					},
+				]}>
+				<Text style={{ color: textColor }}>{title}</Text>
+			</TouchableOpacity>
+		);
+	};
+
+	return (
+		<View style={styles.container}>
+			<CustomButton
+				title='Login'
+				onPress={() => console.log('login')}
+				bgColor='#efefef'
+				textColor='#000'
+			/>
+			<CustomButton title='LogOut' onPress={() => console.log('LogOut')} />
+			<CustomButton title='Pay' onPress={() => console.log('Pay')} />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	btn: {
+		padding: 10,
+		borderRadius: 5,
+		width: 150,
+		height: 50,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
 });
